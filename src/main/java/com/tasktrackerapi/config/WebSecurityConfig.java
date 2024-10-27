f4cce1556a352f14ca5841e0f3969c2422fef7e9
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-27 11:47:57 +0300

    Create basic security.

---
+++ src/main/java/com/tasktrackerapi/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.tasktrackerapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                                .loginProcessingUrl("login")
+                                .permitAll()
+
+                        )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+        AuthenticationManagerBuilder manager = http.getSharedObject(AuthenticationManagerBuilder.class);
+        manager.authenticationProvider(provider());
+        return manager.build();
+    }
+    @Bean
+    public DaoAuthenticationProvider provider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
