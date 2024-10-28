4a2dc160cf2b00d999d412f534e6f59b035f6df2
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-28 09:42:27 +0300

    feat: implement service layer logic for project.

--- src/main/java/com/tasktrackerapi/service/ProjectService.java
+++ src/main/java/com/tasktrackerapi/service/ProjectService.java
@@ -1,10 +1,15 @@
 package com.tasktrackerapi.service;
 
 import com.tasktrackerapi.dto.ProjectAddDTO;
+import com.tasktrackerapi.dto.ProjectGetDTO;
+
+import java.util.List;
 
 public interface ProjectService {
 
     void addProject(ProjectAddDTO projectAddDTO);
 
     void deleteProject(Long projectId);
+
+    List<ProjectGetDTO> getAllProjects();
 }
--- src/main/java/com/tasktrackerapi/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/tasktrackerapi/service/impl/ProjectServiceImpl.java
@@ -1,20 +1,46 @@
 package com.tasktrackerapi.service.impl;
 
 import com.tasktrackerapi.dto.ProjectAddDTO;
+import com.tasktrackerapi.dto.ProjectGetDTO;
+import com.tasktrackerapi.dto.mapper.ProjectGetDTOMapper;
+import com.tasktrackerapi.model.Project;
+import com.tasktrackerapi.repository.ProjectRepository;
 import com.tasktrackerapi.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    private final ProjectGetDTOMapper projectGetDTOMapper;
+
     @Override
     public void addProject(ProjectAddDTO projectAddDTO) {
-
+        Project project = new Project();
+        project.setName(projectAddDTO.name());
+        projectRepository.save(project);
     }
 
     @Override
     public void deleteProject(Long projectId) {
+        if (projectRepository.existsById(projectId)) {
+            projectRepository.deleteById(projectId);
+        } else {
+            throw new IllegalArgumentException("Project not found with ID: " + projectId);
+        }
+    }
 
+    @Override
+    public List<ProjectGetDTO> getAllProjects() {
+        return projectRepository.findAll()
+                .stream()
+                .map(projectGetDTOMapper)
+                .collect(Collectors.toList());
     }
 }
